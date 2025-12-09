4e058c094236c41b161a3b01a7a33abb59679ac4
Aldo Vargas <aldofunado@gmail.com>
2025-12-08 20:57:26 -0600

    Se creo la clase pilas

--- src/JuegoAhorcado.java
+++ src/JuegoAhorcado.java
@@ -12,7 +12,41 @@ class Oportunidades{
 	public int obtenerOportunidades() {
 		return this.oportunidades;
 	}
-}
+}//class oportunidades
+
+class PilaLetras{
+	private char[] datos;
+	private int tope;
+	
+	public PilaLetras(int capacidad) {
+		datos = new char [capacidad];
+		tope = -1;
+	}
+	public boolean vacio() {
+		return tope == -1;
+	}
+	public boolean lleno() {
+		return tope == datos.length-1;
+	}
+	public void push(char cima) {
+		if(!lleno()) {
+			datos[++tope] = cima;
+		}
+	}
+	public char pop() {
+		if(!vacio()) {
+			return datos[tope--];
+		}
+		return '\0';
+	}
+	public void mostrarPila() {
+		System.out.println("Letras ingresadas: ");
+		for (int i = tope; i >= 0; i--) {
+			System.out.println(datos[i] + "");
+		}
+		System.out.println("\n");
+	}
+}//class pila
 
 public class JuegoAhorcado {
 
