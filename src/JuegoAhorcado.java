d5ca47b6f15613e88f1c75fe957f6ef6cc80658b
Aldo Vargas <aldofunado@gmail.com>
2025-12-08 19:45:52 -0600

    Se creo la clase oportunidades

--- src/JuegoAhorcado.java
+++ src/JuegoAhorcado.java
@@ -1,8 +1,23 @@
+class Oportunidades{
+	int oportunidades = 0;
+	
+	public void iniciarOportunidades() {
+		this.oportunidades = 10;
+	}
+	public void reducriOp() {
+		if(this.oportunidades > 0) {
+			this.oportunidades--;
+		}
+	}
+	public int obtenerOportunidades() {
+		return this.oportunidades;
+	}
+}
 
 public class JuegoAhorcado {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
